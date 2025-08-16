e07dd34eb54aafe4801d91e090fc7a1f601702b5
seamus-xray <shane.a.sullivan@gmail.com>
2025-08-16 19:03:11 +1000

    Update filebrowser-lxc.sh

--- filebrowser-lxc.sh
+++ filebrowser-lxc.sh
@@ -76,8 +76,7 @@ sleep 5
 echo -e "${info} Installing cURL ..."
 pct exec $CTID -- bash -c "apt update && apt install -y curl"
 sleep 5
-pct exec $CTID --interactive --tty --bash
 echo -e "${INFO} Installing FileBrowser in container..."
-pct exec $CTID -- bash -c "curl -fsSL https://raw.githubusercontent.com/seamus-xray/proxmox-filebrowser-lxc/main/filebrowser-install.sh | bash"
+PASSWORD="$PASSWORD" pct exec $CTID -- bash -c "curl -fsSL https://raw.githubusercontent.com/seamus-xray/proxmox-filebrowser-lxc/main/filebrowser-install.sh | bash"
 
 echo -e "${CM} FileBrowser LXC deployed and installation started."
